bb7dd8fca2f7ad9e7c260750ee33c46f9647cb24
root <root@raspberrypi.(none)>
2016-09-24 15:00:56 +0000

    Improved rain sensor

--- rainsensor.py
+++ rainsensor.py
@@ -4,14 +4,40 @@ from time import sleep
 import os
 import RPi.GPIO as GPIO
 import requests
+import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN)
-state = GPIO.input(4)
+isRaining = False
+supposedStoppedRainingTimestamp = 0
+GPIO_NUMBER = 4
+WS_URL = 'http://erp.librairielabourse.fr/postcall/rainsensor.postcall.php'
 
-while true:
+while True:
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_NUMBER, GPIO.IN)
+    state = GPIO.input(GPIO_NUMBER)
+
     if state == 0:
-        print "Water detected!"
-        r = requests.post("http://erp.librairielabourse.fr/postcall/rainsensor.postcall.php")
+
+        print "Water detected !"
+
+        if isRaining == False:
+            print "Calling server..."
+            isRaining = True;
+            requests.post(WS_URL, data = {})
+            sleep(5)
     else:
-        print "Water not detected"
+        print "Water not detected..."
+
+        if isRaining == True:
+            print "It maybe stopped raining... let's wait some time."
+            supposedStoppedRainingTimestamp = time.time()
+            isRaining = False        
+ 
+        if supposedStoppedRainingTimestamp != 0 and time.time() > (supposedStoppedRainingTimestamp + 5):
+            print "It surely stopped raining. Calling server..."
+            requests.post(WS_URL, data = {'stoppedRaining' : True})
+            supposedStoppedRainingTimestamp = 0
+
+        sleep(1)
+
